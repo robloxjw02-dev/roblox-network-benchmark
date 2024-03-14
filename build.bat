a5632edef38b0e0fb026fd959453c7ebdf66b676
Jo Project <jo.project.0911@gmail.com>
2024-03-14 17:53:57 +0700

    feat: Added auto download tools and auto run roblox place

--- build.bat
+++ build.bat
@@ -1,19 +1,33 @@
 @echo off
 
+setlocal
+:PROMPT
+SET "AREYOUSURE=N"
+SET /P "AREYOUSURE=Download tools? (Y/[N])?"
+IF /I %AREYOUSURE% NEQ Y GOTO COMPILE
+
+echo Downloading dependencies...
+rmdir /s /q ".\tools"
+rmdir /s /q ".\packages"
+mkdir ".\tools"
+mkdir ".\packages"
+lune run download
+del /q ".\tools\*.zip"
+del /q ".\packages\*.zip"
+
 :COMPILE
 echo Compiling definitions
-if exist "./src/shared/speedy" rmdir "./src/shared/speedy"
-if exist "./src/shared/swiftlink" rmdir "./src/shared/swiftlink"
-if exist "./Benchmark.rbxl" rmdir "./Benchmark.rbxl"
+if exist "./src/shared/speedy" rmdir /s /q "./src/shared/speedy"
+if exist "./src/shared/blitz" rmdir /s /q "./src/shared/blitz"
 
 mkdir "./src/shared/speedy"
-mkdir "./src/shared/swiftlink"
+mkdir "./src/shared/blitz"
 "./tools/speedy.exe" "./definitions/definition.speedy"
-"./tools/swift-link.exe" "./definitions/definition.swfl"
+"./tools/blitz.exe" "./definitions/definition.blitz"
 endlocal
 
 echo Building ROBLOX place
-if exist out rmdir out
+if exist out rmdir /s /q  out
 if exist "./Benchmark.rbxl" del "./Benchmark.rbxl"
 mkdir out
 rojo sourcemap default.project.json --output sourcemap.json --include-non-scripts
@@ -26,4 +40,7 @@ SET "OPENSTUDIO=N"
 SET /P OPENSTUDIO=Open generated place? (Y/[N])?
 if /I %OPENSTUDIO% NEQ Y GOTO END
 
-start Benchmark.rbxl
+lune run generate
+
+:END
+endlocal
